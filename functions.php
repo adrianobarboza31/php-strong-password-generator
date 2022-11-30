b5c6390eec6060ccc3a6216719a3a54f99be6356
adrianobarboza31 <barbozaa022@gmail.com>
2022-11-30 10:10:48 +0100

    add bonus

--- functions.php
+++ functions.php
@@ -1,12 +1,28 @@
 <?php
 
-function randompassword($lunghezza){
+function randompassword($lunghezza,$numero,$lettera,$maiuscola,$simbolo){
+  
     $numeri='12345678';
     $lettere='abcdefg';
     $maiuscole='ABCDEFG';
     $simboli='?.:!+-=';
+    if($numero==1){
+        $numero=$numeri;
+     }
+     if($lettera==2){
+        $lettera=$lettere;
+     }
+     if($maiuscola==3){
+        $maiuscola=$maiuscole;
+     }
+     if($simbolo==4){
+        $simbolo=$simboli;
+     }
     $password='';
-    $pass_noflitru=$numeri.$lettere.$maiuscole.$simboli ;
+    $pass_noflitru=$numero.$lettera.$maiuscola.$simbolo ;
+    if($pass_noflitru==''){
+        $pass_noflitru=$numeri.$lettere.$maiuscole.$simboli ;
+    }
     while(strlen($password)+1<=$lunghezza){
                 $indice=rand(0,strlen($pass_noflitru) -1);
                 $password .=$pass_noflitru[$indice];
--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
  session_start();
+
+
  $lunghezza=($_GET['numero']);
+ $numero=($_GET['numeri']);
+ $lettera=($_GET['lettere']);
+ $maiuscola=($_GET['maiuscole']);
+ $simbolo=($_GET['simboli']);
  $_SESSION['lung'] = $lunghezza;
 include __DIR__ .'/functions.php';
 if(isset($_GET['numero']) && !empty($_GET['numero'])) {
-    $password=randompassword($_GET['numero']);
+    $password=randompassword(($_GET['numero']),($_GET['numeri']),($_GET['lettere']),($_GET['maiuscole']),($_GET['simboli']));
     $_SESSION['pass'] = $password;
 };
 
@@ -23,6 +29,10 @@ if(isset($_GET['numero']) && !empty($_GET['numero'])) {
     <input type="number" name="numero" id="numero">
     <button type="submit">invia</button>
     <button type="reset">reset</button>
+    <input type="radio" name="numeri" value=1> <label for="">numeri</label>
+    <input type="radio" name="lettere"  value=2><label for="">lettere</label>
+    <input type="radio" name="maiuscole"  value=3><label for="">maiuscole</label>
+    <input type="radio" name="simboli"  value=4><label for="">simboli</label>
 </form>
     <!-- -->
     <a href="session.php">clicca il link per vedere la password</a>
